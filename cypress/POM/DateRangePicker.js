d85d3a90b0541e7cfc5f6fc3c64a0f2a152a3e50
Nishant324 <nishant.sirothiya@staxpayments.com>
2023-06-30 20:11:24 +0530

    Refactor the code for better data management

---
+++ cypress/POM/DateRangePicker.js
@@ -0,0 +1,14 @@
+class DateRangePicker {
+
+   selectDateRange(startDate) {
+    cy.get('#mui-4')
+    }
+
+   selectEndDateRange(endDate){
+    cy.get('#mui-5')
+   }
+
+   
+    // You can add other helper methods specific to this page
+  }
+  
