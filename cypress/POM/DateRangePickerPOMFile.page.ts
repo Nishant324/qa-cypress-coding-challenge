392f15ecb7c766b3156ab32dd02657ee1b6433dc
Nishant324 <nishant.sirothiya@staxpayments.com>
2023-07-01 13:25:09 +0530

    Updated the approach

---
+++ cypress/POM/DateRangePickerPOMFile.page.ts
@@ -0,0 +1,31 @@
+class DateRangePickerPage {
+  
+  
+    getDateRangePicker() {
+      return cy.get('.DateRangeSelect');
+    }
+  
+    openStartDatePicker() {
+      return cy.get('#mui-4');
+    }
+
+    openEndDatePicker() {
+      return cy.get('#mui-5');
+    }
+  
+    getDatePicker() {
+      return cy.get('.css-1xhj18k');
+    }
+  
+    clickForwardArrow() {
+      return cy.xpath('(//*[name()="svg"])[1]');
+    }
+  
+    clickBackwardArrow() {
+      return cy.xpath('(//*[name()="svg"])[4]');
+    }
+  
+  }
+  
+  export default DateRangePickerPage;
+  
